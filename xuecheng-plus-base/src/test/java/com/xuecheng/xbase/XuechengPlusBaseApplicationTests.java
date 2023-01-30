11a2a9ebb264305defb91072c939b31abe87d71e
碎魂_ <qq591825996@gmail.com>
2023-01-30 18:12:24 +0800

    创建基础工程

---
+++ xuecheng-plus-base/src/test/java/com/xuecheng/xbase/XuechengPlusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng.xbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
